7e4c9f448f9e5e1d14f757080608141d74b5ccd3
ras19 <ras19@ic.ac.uk>
2023-04-09 13:00:23 +0100

    Trying to find a way to extract labels from two approaches

--- envRandomExploration.py
+++ envRandomExploration.py
@@ -22,14 +22,15 @@ def choose_action():
 # Extracts the n shortest successful traces, where n is no_of_traces_to_extract
 def extractShortestSuccessfulTraces(trace_lengths, states, events, no_of_traces_to_extract):
     sorted_trace_lengths = sorted(trace_lengths)
-    print(sorted_trace_lengths)
     extracted_state_seqs = []
     extracted_events = []
+    extracted_ep_durations = []
     for i in range(no_of_traces_to_extract):
-        (_, index) = sorted_trace_lengths[i]
+        (length, index) = sorted_trace_lengths[i]
         extracted_state_seqs.append(states[index])
         extracted_events.append(events[index])
-    return extracted_state_seqs, extracted_events
+        extracted_ep_durations.append(length)
+    return extracted_ep_durations, extracted_state_seqs, extracted_events
 
 # Runs an agent in the given environment for a given number of episodes, keeping track of the states traversed and events observed at every time step
 def runAgentWithPolicy(env, num_episodes):
@@ -76,25 +77,25 @@ def encodeStateSeqs(qbn, state_seqs):
 
 # Calculates the cosine similarity score between any pair of state vectors
 # INCOMPLETE
-def extractSimilarStates(state_seqs, sim_threshold):
-    for i in range(len(state_seqs)):
-        cos_sims_per_j = [[[] for y in range(len(state_seqs[x]))] for x in range(len(state_seqs))]
-        for j in range(len(state_seqs)):
-            if (i != j):
-                state_seq_i = state_seqs[i]
-                state_seq_j = state_seqs[j]
-                cos_sims = []
-                for state_i in range(state_seq_i):
-                    cos_sims_i = []
-                    for state_j in range(state_seq_j):
-                        cos_sim = cosine_similarity(state_i, state_j)
-                        cos_sims_i.append(cos_sim)
-                    cos_sims.append(cos_sims_i)
-                cos_sims_per_j[j] = cos_sims
+# def extractSimilarStates(state_seqs, sim_threshold):
+#     for i in range(len(state_seqs)):
+#         cos_sims_per_j = [[[] for y in range(len(state_seqs[x]))] for x in range(len(state_seqs))]
+#         for j in range(len(state_seqs)):
+#             if (i != j):
+#                 state_seq_i = state_seqs[i]
+#                 state_seq_j = state_seqs[j]
+#                 cos_sims = []
+#                 for state_i in range(state_seq_i):
+#                     cos_sims_i = []
+#                     for state_j in range(state_seq_j):
+#                         cos_sim = cosine_similarity(state_i, state_j)
+#                         cos_sims_i.append(cos_sim)
+#                     cos_sims.append(cos_sims_i)
+#                 cos_sims_per_j[j] = cos_sims
 
 # Calculates the cosine similarity score between two vectors and records the indices of states that are similar enough (where enough is determined by a threshold score provided as input)
 def extractSimilarStates(state_seqs, sim_threshold):
-    similar_states_indices = []
+    similar_states_indices = [[[] for _ in range(len(state_seqs[x]))] for x in range(len(state_seqs))]
     for i in range(len(state_seqs)):
         for j in range(len(state_seqs)):
             state_seq_i = state_seqs[i]
@@ -102,33 +103,26 @@ def extractSimilarStates(state_seqs, sim_threshold):
             print(len(state_seq_i))
             print(len(state_seq_j))
             if (state_seq_i != state_seq_j):
+                cos_sims = cosine_similarity(state_seq_i, state_seq_j)
                 for x in range(len(state_seq_i)):
                     for y in range(len(state_seq_j)):
-                        state_x = np.array([state_seq_i[x]])
-                        state_y = np.array([state_seq_j[y]])
-                        cos_sim = cosine_similarity(state_x, state_y)
+                        # state_x = [state_seq_i[x]]
+                        # state_y = [state_seq_j[y]]
+                        cos_sim = cos_sims[x][y]
                         if (cos_sim >= sim_threshold):
-                            similar_states_indices.append(((i, x), (j, y)))
+                            similar_states_indices[i][x].append((j, y, cos_sim))
     return similar_states_indices
 
 # Uses principal component analysis to reduce the dimensionality of the data into two dimensions. This will help for visualisation of the KMeans clustering
-def datasetDimReduction(state_seqs):
-    # print(state_seqs)
-    flattened_states = np.concatenate(state_seqs)
+def datasetDimReduction(states):
     pca2 = PCA(n_components=2)
-    print(flattened_states)
-    principal_components = pca2.fit_transform(flattened_states)
+    principal_components = pca2.fit_transform(states)
     return principal_components
 
 # Plots the KMeans clusters
 def plotKMeansClustering(df, labels, no_of_clusters):
-    print("Labels (corresponding to different clusters)")
-    print(labels)
-    # print(df)
     for i in range(no_of_clusters):
         filtered_df = df[labels == i]
-        print("Filtered dataframe for label {}".format(i))
-        print(filtered_df)
         plt.scatter(filtered_df['Principal component 1'], filtered_df['Principal component 2'], label = i)
     plt.legend()
     plt.show()
@@ -139,7 +133,8 @@ def kmeansClustering(state_seqs, no_of_clusters):
     pca_df = pd.DataFrame(data=principal_components, columns=['Principal component 1', 'Principal component 2'])
     kmeans = KMeans(n_clusters=no_of_clusters)
     labels = kmeans.fit_predict(pca_df)
-    plotKMeansClustering(pca_df, labels, no_of_clusters)
+    # plotKMeansClustering(pca_df, labels, no_of_clusters)
+    return labels
 
 if __name__ == '__main__':
     env = gym.make("gym_subgoal_automata:WaterWorldRedGreen-v0", params={"generation": "random", "environment_seed": 0})
@@ -150,10 +145,11 @@ if __name__ == '__main__':
 
     # Generate successful traces for the task
     episode_durations, states_traversed, episode_events = runAgentWithPolicy(env, NUM_EPISODES)
+    print("Episode durations")
+    print(episode_durations)
 
     # Get the shortest traces- they are the most relevant
-    relevant_state_seqs, relevant_events = extractShortestSuccessfulTraces(episode_durations, states_traversed, episode_events, NUM_SUCC_TRACES)
-    # print(relevant_state_seqs)
+    shortest_ep_durations, relevant_state_seqs, relevant_events = extractShortestSuccessfulTraces(episode_durations, states_traversed, episode_events, NUM_SUCC_TRACES)
 
     # Load the QBN (trained through the program qbnTrainAndEval.py)
     input_vec_dim = 52
@@ -174,20 +170,42 @@ if __name__ == '__main__':
     encoded_seqs = encodeStateSeqs(qbn, relevant_state_seqs)
 
     # Extract labels from latent vectors through pairwise comparison with cosine similarity
-    # print("Applying cosine similarity to each pair")
-    # sim_threshold = 0.82
-    # sim_states_indices = extractSimilarStates(encoded_seqs, sim_threshold)
+    print("Applying cosine similarity to each pair")
+    sim_threshold = 0.82
+    sim_states_indices = extractSimilarStates(encoded_seqs, sim_threshold)
     # TODO: Extract labels from similar states
-    # print("Indices of similar states. Similarity score threshold: {}".format(sim_threshold))
-    # print(sim_states_indices)
+    print("Indices of similar states. Similarity score threshold: {}".format(sim_threshold))
+    for i in range(NUM_SUCC_TRACES):
+        for j in range(len(sim_states_indices[i])):
+            print("Vectors similar to the {}th state in the {}th trace:".format(j, i))
+            print(sim_states_indices[i][j])
+            print("-------------")
+        print("----------------------------------------------")
     # print(len(sim_states_indices))
 
     # Alternatively, extract labels from latent vectors with k-means clustering
-    print("Clustering the state sequences with KMeans and PCA")
-    no_of_events = 2
-    kmeansClustering(encoded_seqs, 2 ** no_of_events)
+    # print("Clustering the state sequences with KMeans and PCA")
+    # no_of_events = 2
+    # cluster_labels = kmeansClustering(np.concatenate(encoded_seqs), 2 ** no_of_events)
+    # print(cluster_labels)
 
     # TODO: Perform evaluation of extracted labels 
+    # event_sets = [{} for _ in range(2 ** no_of_events)]
+    # i = 0
+    # j = 0
+    # k = 0
+    # while i < NUM_SUCC_TRACES:
+    #     label = cluster_labels[k]
+    #     event = relevant_events[i][j]
+    #     event_sets[label].add(event)
+    #     j += 1
+    #     k += 1
+    #     if j >= shortest_ep_durations[i]:
+    #         i += 1
+    #         j = 0
+
+    # print(event_sets)
+
     # for ((i, x), (j, y)) in sim_states_indices:
     #     # print("({}, {}), ({}, {})".format(i, x, j, y))
     #     events_observed_1 = relevant_events[i][x]
