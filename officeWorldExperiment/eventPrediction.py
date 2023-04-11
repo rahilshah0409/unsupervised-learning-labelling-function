514417317f0d2b4593a4f409fa8e4ce0b36fd580
ras19 <ras19@ic.ac.uk>
2023-04-11 14:54:47 +0100

    Tried experimenting with office world environment. Random policy makes it not viable

--- officeWorldExperiment/eventPrediction.py
+++ officeWorldExperiment/eventPrediction.py
@@ -8,11 +8,9 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import PCA
 
-from qbn import QuantisedBottleneckNetwork
-
 # Defines a policy for an agent to choose actions at each timestep. Initially random
 def choose_action():
-    NUM_ACTIONS = 5
+    NUM_ACTIONS = 4
     action_probs = np.full(NUM_ACTIONS, 1 / NUM_ACTIONS)
     action = random.choices(range(NUM_ACTIONS), weights=action_probs, k=1)[0]
     return action
@@ -24,54 +22,44 @@ def run_agent(env, num_episodes):
     events_per_episode = []
     for ep in range(num_episodes):
         print("Episode {} in progress".format(ep + 1))
-        
-        initial_state = env.reset()
-        state = torch.tensor(initial_state).float()
         states = []
+        state = env.reset()
         states.append(state)
+        # How would you get the event that is observed in the initial state?
         events_observed = [set()]
-        done, terminated, t = False, False, 1
-        while not (done or terminated):
+        done, t = False, 1
+        while not done:
             action = choose_action()
-            next_state, _, done, observations = env.step(action)
-            next_state = torch.tensor(next_state).reshape(-1).float()
+            next_state, reward, done, observations = env.step(action)
             state = next_state
             states.append(state)
             t += 1
 
-            # if (observations != set()): 
             events_observed.append(observations)
             if done:
-                episode_durations.append((t, ep))
-            
+                episode_durations.append((t, ep, reward == 1.0))
 
         states_traversed.append(states)
         events_per_episode.append(events_observed)
     return episode_durations, states_traversed, events_per_episode
 
 # Extracts the n shortest successful traces, where n is no_of_traces_to_extract
-def extract_shortest_succ_traces(trace_lengths, states, events, no_of_traces_to_extract):
-    sorted_trace_lengths = sorted(trace_lengths)
-    extracted_state_seqs = []
-    extracted_events = []
-    extracted_ep_durations = []
-    for i in range(no_of_traces_to_extract):
-        (length, index) = sorted_trace_lengths[i]
-        extracted_state_seqs.append(states[index])
-        extracted_events.append(events[index])
-        extracted_ep_durations.append(length)
-    return extracted_ep_durations, extracted_state_seqs, extracted_events
-
-# Given a list of state sequences, encodes every state in every sequence with the QBN given
-def encode_state_seqs(qbn, state_seqs):
-    encoded_seqs = []
-    for state_seq in state_seqs:
-        encoded_state_seq = []
-        for state in state_seq:
-            encoded_state = qbn.encode(state)
-            encoded_state_seq.append(encoded_state.detach().numpy())        
-        encoded_seqs.append(encoded_state_seq)
-    return encoded_seqs
+def get_relevant_succ_traces(trace_lengths, states, events, no_of_traces_to_extract):
+    succ_traces = list(filter(lambda trace_info: trace_info[2], trace_lengths))
+    sorted_trace_lengths = sorted(succ_traces)
+    relevant_state_seqs = []
+    relevant_events = []
+    relevant_ep_lens = []
+    if no_of_traces_to_extract < len(sorted_trace_lengths):
+        for i in range(no_of_traces_to_extract):
+            (length, index, _) = sorted_trace_lengths[i]
+            relevant_state_seqs.append(states[index])
+            relevant_events.append(events[index])
+            relevant_ep_lens.append(length)
+    else:
+        print("There is not enough traces! There is only {} traces".format(
+            len(sorted_trace_lengths)))
+    return relevant_ep_lens, relevant_state_seqs, relevant_events
 
 # Uses principal component analysis to reduce the dimensionality of the data into two dimensions. This will help for visualisation of the KMeans clustering
 def dataset_dim_reduction(states):
@@ -83,14 +71,16 @@ def dataset_dim_reduction(states):
 def plot_kmeans_clustering(df, labels, no_of_clusters):
     for i in range(no_of_clusters):
         filtered_df = df[labels == i]
-        plt.scatter(filtered_df['Principal component 1'], filtered_df['Principal component 2'], label = i)
+        plt.scatter(filtered_df['Principal component 1'],
+                    filtered_df['Principal component 2'], label=i)
     plt.legend()
     plt.show()
 
 # Performs KMeans clustering on the state sequences and plots the resulting data
 def kmeans_clustering(state_seqs, no_of_clusters):
     principal_components = dataset_dim_reduction(state_seqs)
-    pca_df = pd.DataFrame(data=principal_components, columns=['Principal component 1', 'Principal component 2'])
+    pca_df = pd.DataFrame(data=principal_components, columns=[
+                          'Principal component 1', 'Principal component 2'])
     kmeans = KMeans(n_clusters=no_of_clusters)
     labels = kmeans.fit_predict(pca_df)
     # plotKMeansClustering(pca_df, labels, no_of_clusters)
@@ -122,8 +112,10 @@ def plot_cluster_labels_over_traces(state_seqs, cluster_labels):
         if state_index_in_trace >= len(state_seqs[succ_trace_index]):
             x_axis = range(state_index_in_trace)
             axs[succ_trace_index].plot(x_axis, y_axis)
-            axs[succ_trace_index].set_title("Trace {}".format(succ_trace_index))
-            axs[succ_trace_index].set(xlabel="State index", ylabel="Cluster labels")
+            axs[succ_trace_index].set_title(
+                "Trace {}".format(succ_trace_index))
+            axs[succ_trace_index].set(
+                xlabel="State index", ylabel="Cluster labels")
             succ_trace_index += 1
             state_index_in_trace = 0
             y_axis = []
@@ -143,7 +135,8 @@ def extract_events_from_clustering(state_seqs):
 
 # Calculates the cosine similarity score between two vectors and records the indices of states that are similar enough (where enough is determined by a threshold score provided as input)
 def extract_sim_states(state_seqs, sim_threshold):
-    similar_states_indices = [[[] for _ in range(len(state_seqs[x]))] for x in range(len(state_seqs))]
+    similar_states_indices = [
+        [[] for _ in range(len(state_seqs[x]))] for x in range(len(state_seqs))]
     for i in range(len(state_seqs)):
         for j in range(len(state_seqs)):
             state_seq_i = state_seqs[i]
@@ -156,14 +149,16 @@ def extract_sim_states(state_seqs, sim_threshold):
                     for y in range(len(state_seq_j)):
                         cos_sim = cos_sims[x][y]
                         if (cos_sim >= sim_threshold):
-                            similar_states_indices[i][x].append((j, y, cos_sim))
+                            similar_states_indices[i][x].append(
+                                (j, y, cos_sim))
     return similar_states_indices
 
 # Extracts event labels by comparing state vectors across different successful traces
 def extract_events_from_pairwise_comp(state_seqs):
     sim_threshold = 0.82
     sim_states_indices = extract_sim_states(state_seqs, sim_threshold)
-    print("Indices of similar states. Similarity score threshold: {}".format(sim_threshold))
+    print("Indices of similar states. Similarity score threshold: {}".format(
+        sim_threshold))
     for i in range(NUM_SUCC_TRACES):
         for j in range(len(sim_states_indices[i])):
             print("Vectors similar to the {}th state in the {}th trace:".format(j, i))
@@ -179,48 +174,37 @@ def extract_events_from_pairwise_comp(state_seqs):
 def extract_events(state_seqs, pairwise_comp):
     return extract_events_from_pairwise_comp(state_seqs) if pairwise_comp else extract_events_from_clustering(state_seqs)
 
+
 if __name__ == '__main__':
-    env = gym.make("gym_subgoal_automata:WaterWorldRedGreen-v0", params={"generation": "random", "environment_seed": 0})
-    trained_model_loc = "./trainedQBN/finalModel.pth"
+    env = gym.make("gym_subgoal_automata:OfficeWorldDeliverCoffee-v0",
+                   params={"generation": "random", "environment_seed": 0})
 
     NUM_SUCC_TRACES = 2
-    NUM_EPISODES = 10
+    NUM_EPISODES = 100
 
     # Generate successful traces for the task
-    episode_durations, states_traversed, episode_events = run_agent(env, NUM_EPISODES)
+    episode_durations, states_traversed, episode_events = run_agent(
+        env, NUM_EPISODES)
 
     # Get the shortest traces- they are the most relevant
-    shortest_ep_durations, relevant_state_seqs, relevant_events = extract_shortest_succ_traces(episode_durations, states_traversed, episode_events, NUM_SUCC_TRACES)
+    shortest_ep_durations, relevant_state_seqs, relevant_events = get_relevant_succ_traces(
+        episode_durations, states_traversed, episode_events, NUM_SUCC_TRACES)
 
-    # Load the QBN (trained through the program qbnTrainAndEval.py)
-    input_vec_dim = 52
-    quant_vector_dim = 100
-    training_batch_size = 32
-    test_batch_size = 32
-    learning_rate = 1e-4
-    weight_decay = 0
-    epochs = 300
-    training_set_size = 8192
-    testing_set_size = 2048
-    qbn = QuantisedBottleneckNetwork(input_vec_dim, quant_vector_dim, training_batch_size, learning_rate, weight_decay, epochs, training_set_size)
-    qbn.load_state_dict(torch.load(trained_model_loc))
-    print("QBN loaded")
-
-    # Encoded every state (tensor object) in every sequence with the QBN
-    print("Using QBN to encode states")
-    encoded_seqs = encode_state_seqs(qbn, relevant_state_seqs)
+    print(shortest_ep_durations)
+    print(relevant_state_seqs)
+    print(relevant_events)
 
     # Transforming each state from a tensor object to np.ndarray
-    relevant_state_seqs = list(map(lambda seq: list(map(lambda state: state.detach().numpy(), seq)), relevant_state_seqs))
+    # relevant_state_seqs = list(map(lambda seq: list(map(lambda state: state.detach().numpy(), seq)), relevant_state_seqs))
 
-    # Alternatively, extract labels from latent vectors with k-means clustering
-    event_labels = extract_events(state_seqs=encoded_seqs, pairwise_comp=False)
+    # # Alternatively, extract labels from latent vectors with k-means clustering
+    # event_labels = extract_events(state_seqs=encoded_seqs, pairwise_comp=False)
 
-    # Perform evaluation of extracted labels 
-    conc_relevant_events = np.concatenate(relevant_events)
-    correct_labels = [l1 for l1, l2 in zip(event_labels, conc_relevant_events) if l1 == l2]
-    accuracy = len(correct_labels) / len(conc_relevant_events)
-    print("Accuracy of predicted mapping of event labels is {}".format(accuracy))
+    # # Perform evaluation of extracted labels
+    # conc_relevant_events = np.concatenate(relevant_events)
+    # correct_labels = [l1 for l1, l2 in zip(event_labels, conc_relevant_events) if l1 == l2]
+    # accuracy = len(correct_labels) / len(conc_relevant_events)
+    # print("Accuracy of predicted mapping of event labels is {}".format(accuracy))
 
     # for ((i, x), (j, y)) in sim_states_indices:
     #     # print("({}, {}), ({}, {})".format(i, x, j, y))
@@ -232,4 +216,3 @@ if __name__ == '__main__':
     #         print(events_observed_1)
     #         print("Events observed 2")
     #         print(events_observed_2)
-
