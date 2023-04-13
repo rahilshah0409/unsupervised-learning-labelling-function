fbd3ec2d12d084df77d3aac1c6012844cd77a477
ras19 <ras19@ic.ac.uk>
2023-04-13 10:25:39 +0100

    Added environment arguments to experiment code to line up with recent refactoring

--- waterWorldExperiment/eventPredExperiments.py
+++ waterWorldExperiment/eventPredExperiments.py
@@ -1,34 +1,39 @@
 import numpy as np
 from eventPredBaseProcess import runEventPrediction
+import gym
 
 
-def vary_no_of_succ_traces(no_of_runs, no_of_succ_traces_arr, no_of_eps):
+def vary_no_of_succ_traces(env, no_of_runs, no_of_succ_traces_arr, no_of_eps):
     accuracy_results = [[] for _ in range(len(no_of_runs))]
     for run in no_of_runs:
         for n in no_of_succ_traces_arr:
-            accuracy = runEventPrediction(n, no_of_eps)
+            accuracy = runEventPrediction(env, n, no_of_eps)
             accuracy_results[run].append(accuracy)
     return accuracy_results
 
 
-def vary_no_of_episodes(no_of_runs, no_of_eps_arr, no_of_succ_traces):
+def vary_no_of_episodes(env, no_of_runs, no_of_eps_arr, no_of_succ_traces):
     accuracy_results = [[] for _ in range(len(no_of_runs))]
     for run in no_of_runs:
         for n in no_of_eps_arr:
-            accuracy = runEventPrediction(no_of_succ_traces, n)
+            accuracy = runEventPrediction(env, no_of_succ_traces, n)
             accuracy_results[run].append(accuracy)
     return accuracy_results
 
 
 if __name__ == "__main__":
     no_of_runs = 10
+    env = gym.make(
+        "gym_subgoal_automata:WaterWorldRedGreen-v0",
+        params={"generation": "random", "environment_seed": 0},
+    )
 
     num_episodes = 500
     num_succ_traces_arr = np.arange(25, 500, 25)
     accuracy_results_1 = vary_no_of_succ_traces(
-        no_of_runs, num_succ_traces_arr, num_episodes)
+        env, no_of_runs, num_succ_traces_arr, num_episodes)
 
     num_episodes_arr = np.arange(100, 600, 100)
     num_succ_traces = 50
     accuracy_results_1 = vary_no_of_episodes(
-        no_of_runs, num_episodes_arr, num_succ_traces)
+        env, no_of_runs, num_episodes_arr, num_succ_traces)
